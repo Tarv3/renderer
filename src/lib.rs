f453cee765b6769d9c0540749524adc4dfd6b0aa
Josh <tarv360@gmail.com>
2018-09-12 21:34:31 +0930

    Added types PV and OV

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@ pub type Vec3 = na::Vector3<f32>;
 pub type Vec2 = na::Vector2<f32>;
 pub type Mat4 = na::Matrix4<f32>;
 pub type Pnt3 = na::Point3<f32>;
+pub type PV = na::Perspective3<f32>;
+pub type OV = na::Orthographic3<f32>;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Vertex {
